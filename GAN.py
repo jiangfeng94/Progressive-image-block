de899cd57694cdf03c9ae2c9ec578be52e3e5825
SalaFeng <675143618@qq.com>
2019-01-19 00:13:44 +0800

    添加test

--- GAN.py
+++ GAN.py
@@ -5,13 +5,13 @@ import torchvision
 from torch.autograd import Variable
 import numpy as np
 class Generator(nn.Module):
-    def __init__(self):
+    def __init__(self,input_size,batchsize):
         super(Generator,self).__init__()
-        self.img_size =32
+        self.img_size =input_size
         self.init_size =self.img_size//4
         self.z_dim =100
         self.channels =3
-        self.batchsize =16
+        self.batchsize =batchsize
         self.l1 = nn.Sequential(nn.Linear(self.z_dim,128*self.init_size**2))
         self.gen =nn.Sequential(
             nn.BatchNorm2d(128),
@@ -32,8 +32,8 @@ class Generator(nn.Module):
         return self.gen(out)
 
 class Discriminator(nn.Module):
-    def __init__(self):
-        self.img_size =32
+    def __init__(self,input_size):
+        self.img_size =input_size
         self.channels =3
         super(Discriminator,self).__init__()
         self.dis =nn.Sequential(
@@ -48,7 +48,7 @@ class Discriminator(nn.Module):
         return self.dis(img)
 
 class WGAN():
-    def __init__(self,D,G,data,config):
+    def __init__(self,D,G,data,config,init_size):
         self.D = D
         self.G = G
         self.data =data
@@ -56,6 +56,7 @@ class WGAN():
         self.gamma = 0.75
         self.lambda_k = 0.001
         self.k = 0.
+        self.input_size =init_size
     def compute_gradient_penalty(self,real_img, fake_img):
         FloatTensor=torch.cuda.FloatTensor
         alpha = FloatTensor(np.random.random((self.config["batchsize"],1,1,1)))
@@ -77,18 +78,19 @@ class WGAN():
     def train(self):
         self.D.cuda()
         self.G.cuda()
+        torch.load('model/g_0.pkl')
+        torch.load('model/d_0.pkl')
         self.optimizer_D = torch.optim.Adam(self.D.parameters(), lr=self.config['lr'], betas=(self.config['b1'], self.config['b2']))
         self.optimizer_G = torch.optim.Adam(self.G.parameters(), lr=self.config['lr'], betas=(self.config['b1'], self.config['b2']))
         FloatTensor=torch.cuda.FloatTensor
         for epoch in range(100):
             for i,(imgs,_) in enumerate(self.data):
-                crop_imgs =imgs[:,:,16:48,16:48]
+                crop_imgs =imgs[:,:,self.input_size*15//2:self.input_size*17//2,self.input_size*15//2:self.input_size*17//2]
                 real_imgs =Variable(crop_imgs.type(FloatTensor))
                 z = Variable(FloatTensor(np.random.normal(0, 1, (self.config['batchsize'], self.config['z_dim']))))
 
                 self.optimizer_D.zero_grad()
                 fake_imgs =self.G(z)
-                print(real_imgs.size(),fake_imgs.size())
                 gradient_penalty =self.compute_gradient_penalty( real_imgs.data, fake_imgs.data)
                 loss_D = -torch.mean(self.D(real_imgs)) + torch.mean(self.D(fake_imgs))+10*gradient_penalty
                 loss_D.backward()
@@ -101,11 +103,10 @@ class WGAN():
                 self.optimizer_G.step()
                 print ("[Epoch %d] [Batch %d/%d] [D loss: %f] [G loss: %f] " % (epoch, i, len( self.data),
                                                             loss_D.item(), loss_G.item()))
-                if i % 20 == 0:
-                    if not os.path.exists('../output'):
-                        os.mkdir('../output')
-                    #torchvision.utils.save_image(imgs, '../output/%d_in.png' % (epoch * len(self.data) + i), nrow=4, normalize=True)
-                    #torchvision.utils.save_image(crop_imgs, '../output/%d_c1.png' % (epoch * len(self.data) + i), nrow=4, normalize=True)
-                    
-                    torchvision.utils.save_image(real_imgs.data, '../output/%d_crop.png' % (epoch * len(self.data) + i), nrow=4, normalize=True)
-                    torchvision.utils.save_image(gen_imgs.data, '../output/%d.png' % (epoch * len(self.data) + i), nrow=4, normalize=True)
+                if i % 100 == 0:
+                    if not os.path.exists('output'):
+                        os.mkdir('output')
+                    torchvision.utils.save_image(real_imgs.data, 'output/%d_crop.png' % (epoch * len(self.data) + i), nrow=4, normalize=True)
+                    torchvision.utils.save_image(gen_imgs.data, 'output/%d.png' % (epoch * len(self.data) + i), nrow=4, normalize=True)
+                    torch.save(self.D, 'model/d_0.pkl')
+                    torch.save(self.G, 'model/g_0.pkl')
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import argparse
 import torch
+import torchvision
 from GAN import Generator,Discriminator,WGAN
-from utils import weights_init
 from dataset import get_dataloader
+import numpy as np
 parse =argparse.ArgumentParser()
 parse.add_argument('--batchsize',type=int,default=16)
 parse.add_argument('--lr',type=float,default=0.0002)
@@ -10,21 +11,30 @@ parse.add_argument('--b1',type=float,default=0.5)
 parse.add_argument('--b2',type=float,default=0.999)
 parse.add_argument('--z_dim',type=int,default=100)
 parse.add_argument('--channels',type=int,default=1)
-parse.add_argument('--img_size',type=int,default=64)
+parse.add_argument('--img_size',type=int,default=512)
 parse.add_argument('--img_path',type=str,default='D:\img_align_celeba')
 opt =parse.parse_args()
 
 
 
-
+def test():
+    g_0 = Generator(opt.img_size//16,opt.batchsize)
+    torch.load('model/g_0.pkl')
+    g_0.eval()
+    z = torch.autograd.Variable(torch.FloatTensor(np.random.normal(0, 1, (opt.batchsize, opt.z_dim))))
+    output = g_0(z)
+    torchvision.utils.save_image(output.data, 'output/test.png' , nrow=4, normalize=True)
 
 if __name__ == "__main__":
-    dataloader =get_dataloader(opt.img_size,opt.batchsize,opt.img_path)
+    data =get_dataloader(opt.img_size,opt.batchsize,opt.img_path)
     config = {k:v for k,v in opt._get_kwargs()}
-    g_0 = Generator()
-    d_0 = Discriminator()
-    gan = WGAN(d_0, g_0, dataloader, config)
+    init_size =opt.img_size//16
+    g_0 = Generator(init_size,opt.batchsize)
+    d_0 = Discriminator(init_size)
+    gan = WGAN(d_0, g_0, data, config,init_size)
     gan.train()
+    test()
+
 
     
             
